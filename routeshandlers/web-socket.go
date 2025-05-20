630a0e69370858ba93dc1e017041fae6e78382cb
Vikas Mehra <vickeyvikasmehra@gmail.com>
2025-05-20 17:44:51 +0530

    update: websocket server broadcast on each client message

--- routeshandlers/web-socket.go
+++ routeshandlers/web-socket.go
@@ -4,6 +4,7 @@ import (
 	"go-multiplayer-quiz-project/models"
 	"net/http"
 	"strconv"
+	"sync"
 
 	"github.com/gin-gonic/gin"
 	"github.com/gorilla/websocket"
@@ -15,12 +16,16 @@ var upgrader = websocket.Upgrader{
 	},
 }
 
-var joinedPlayers = make(map[int][]*websocket.Conn)
+var (
+	joinedPlayers  = make(map[int][]*websocket.Conn)
+	mu             sync.RWMutex
+	broadcastChans = make(map[int]chan string)
+)
 
 func webSocketLobby(context *gin.Context) {
 	quizId, err := strconv.Atoi(context.Param("id"))
 	if err != nil {
-		context.JSON(http.StatusInternalServerError, gin.H{"message": "could not find the room"})
+		context.JSON(http.StatusInternalServerError, gin.H{"message": err.Error()})
 		return
 	}
 
@@ -29,14 +34,43 @@ func webSocketLobby(context *gin.Context) {
 
 	conn, err := upgrader.Upgrade(context.Writer, context.Request, nil)
 	if err != nil {
-		context.JSON(http.StatusInternalServerError, gin.H{"message": "could not find the room"})
+		context.JSON(http.StatusInternalServerError, gin.H{"message": err.Error()})
 		return
 	}
 
+	mu.Lock()
 	joinedPlayers[quizId] = append(joinedPlayers[quizId], conn)
+	_, exists := broadcastChans[quizId]
+	if !exists { //means host has joined
+		broadcastChans[quizId] = make(chan string)
+		go func() {
 
-	for _, connection := range joinedPlayers[quizId] {
-		connection.WriteJSON(quizRoom.Players)
+			for {
+
+				msg := <-broadcastChans[quizId]
+
+				mu.RLock()
+				conns := append([]*websocket.Conn{}, joinedPlayers[quizId]...)
+				mu.RUnlock()
+
+				for _, connection := range conns {
+					connection.WriteJSON(gin.H{"type": msg,
+						"players": quizRoom.Players})
+				}
+			}
+		}()
 	}
+	mu.Unlock()
+
+	go func() {
+		for {
+			_, msg, err := conn.ReadMessage()
+			if err != nil {
+				context.JSON(http.StatusInternalServerError, gin.H{"message": err.Error()})
+				return
+			}
+			broadcastChans[quizId] <- string(msg)
+		}
+	}()
 
 }
